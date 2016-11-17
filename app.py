2847a648d667059700f83c668310413ee37ab3fe
Soe Htet <soehtet@Soes-MacBook-Pro.local>
2016-11-17 16:25:37 +0800

    add database

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
+import os
+
 from resources.item import Item, ItemList
 from resources.user import UserRegister
 from security import authenticate,identity
 from resources.store import StoreList,Store
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "soe"
 api = Api(app)
