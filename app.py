39a6818e9f53acd10b54d5df20a0e09025fc1448
Soe Htet <soehtet@Soes-MacBook-Pro.local>
2016-11-17 16:06:33 +0800

    solved h error

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.secret_key = "soe"
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister,'/register')
@@ -29,5 +25,5 @@ api.add_resource(StoreList,'/stores')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-app.run(port=5001)
+    app.run(port=5001)
 
